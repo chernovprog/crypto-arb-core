1b6e8b61d4a7d3b6a1283bd5736b9a84f30f950e
Andrii <andrii.chernov@gmail.com>
2025-12-22 16:02:01 +0200

    chore: add Spring Boot initial project structure using Spring Initializr

---
+++ src/main/java/com/achernov/cryptoarb/CryptoArbCoreApplication.java
@@ -0,0 +1,13 @@
+package com.achernov.cryptoarb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoArbCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoArbCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/achernov/cryptoarb/CryptoArbCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achernov.cryptoarb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoArbCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
