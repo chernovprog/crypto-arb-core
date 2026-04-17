02129b018335ffb7d2b2c59c05b412bea79e2ce0
Andrii <andrii.chernov@gmail.com>
2026-04-17 13:07:10 +0300

    feat: register formatter for metadata enums in WebMvcConfigurer

--- src/main/java/com/achernov/cryptoarb/config/WebConfig.java
+++ src/main/java/com/achernov/cryptoarb/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.achernov.cryptoarb.config;
 
+import com.achernov.cryptoarb.dto.metadata.MetadataSection;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/{path:[^.]*}")
             .setViewName("forward:/index.html");
+  }
+
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(String.class, MetadataSection.class, MetadataSection::fromValue);
   }
 }
