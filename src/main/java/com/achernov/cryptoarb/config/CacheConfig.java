2db79597e96507e9948d5c0447902c65a6cadc54
Andrii <andrii.chernov@gmail.com>
2026-04-17 13:02:08 +0300

    feat: configure CacheManager bean

---
+++ src/main/java/com/achernov/cryptoarb/config/CacheConfig.java
@@ -0,0 +1,19 @@
+package com.achernov.cryptoarb.config;
+
+import com.achernov.cryptoarb.constants.CacheNames;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class CacheConfig {
+
+  @Bean
+  public CacheManager cacheManager() {
+    return new ConcurrentMapCacheManager(
+            CacheNames.SYSTEM_METADATA
+    );
+  }
+}
